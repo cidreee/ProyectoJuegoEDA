db6771a011eeaf241778cb40dcf4ec8a27a2bea1
Christopher Oswaldo Márquez Reyes <145897791+Nemesis1174@users.noreply.github.com>
2023-11-30 12:56:00 -0600

    Update Juego.py
    Agregamos la clase mapa que nos ayuda a calcular la distancia minima entre los diferentes lugares del mapa, una funcion para definir las coordenadas de los lugares preestablecidos, además, modificamos el constructor del jugador, agregando la capacidad de recoger y almacenar armas, asi como la posibilidad de tener efectos como el bullet proof, su localización del mapa y una funcion extra que actualiza la posicion del jugador en tiempo real

--- Juego.py
+++ Juego.py
@@ -3,88 +3,98 @@
 @author: Valeria Marian Andrade Monreal
 """
 
-
+import heapq
 import sys
+import time
 
 
+# ---------------------------- Clases del código --------------------------------------------------------
+class Mapa:
+    def __init__(self, pos_actual):
+        self.matriz = [[2, 5, 1, 7],
+                       [13, 8, 11, 10],
+                       [6, 9, 10, 8],
+                       [12, 3, 1, 7]]
+        self.pos_actual = pos_actual
+
+    def calcular_min_costo(self, destino):
+        m = len(self.matriz)
+        n = len(self.matriz[0])
+        dp = [[float('inf') for _ in range(n)] for _ in range(m)]
+        movimientos = [(0, 1), (0, -1), (1, 0), (-1, 0)]  # derecha, izquierda, abajo, arriba
+
+        dp[self.pos_actual[0]][self.pos_actual[1]] = self.matriz[self.pos_actual[0]][self.pos_actual[1]]
+        heap = [(dp[self.pos_actual[0]][self.pos_actual[1]], self.pos_actual)]
+
+        while heap:
+            costo_actual, (i, j) = heapq.heappop(heap)
+
+            if costo_actual != dp[i][j]:
+                continue
+
+            for dx, dy in movimientos:
+                x, y = i + dx, j + dy
+
+                if 0 <= x < m and 0 <= y < n:
+                    costo_nuevo = costo_actual + self.matriz[x][y]
+
+                    if costo_nuevo < dp[x][y]:
+                        dp[x][y] = costo_nuevo
+                        heapq.heappush(heap, (costo_nuevo, (x, y)))
+
+        min_costo = dp[destino[0]][destino[1]]
+        return min_costo
+
+    # Determinamos las coordenadas de cada lugar en el mapa
+    def obtener_posicion_lugar(self, lugar):
+        lugares = {
+            'Lugar1': (3, 0),
+            'Lugar2': (0, 1),
+            'Lugar3': (0, 2),
+            'Lugar4': (0, 3),
+            'Lugar5': (1, 0),
+            # Añade más lugares según sea necesario
+        }
+        return lugares.get(lugar)
+
+
+# Inicializamos los atributos del personaje principal
 class Player:
-    def __init__(self, name='', hp=0, location='start', status_effects=None, items=None):
+    def __init__(self, name='', hp=100, location=(0, 0), status_effects=None, items=None, money=0, bulletproof = False, gun = False, weapons = []):
         self.name = name
+        self.weapons = weapons if weapons is not None else []
+        self.bulletproof = bulletproof
+        self.gun = gun
         self.hp = hp
         self.location = location
         self.status_effects = status_effects if status_effects is not None else []
         self.items = items if items is not None else []
+        self.money = money
+        self.mapa = Mapa(location)
+
+    def add_weapon(self, weapon):
+        self.weapons.append(weapon)
+    
+    def get_weapons(self):
+        for weapon in self.weapons:
+            print(f"\n    - {weapon}\n")
+
+    # Imprimimos la lista de items
+    def get_items(self):
+        return self.items
+
+    # Agregamos un item al inventario
+    def add_item(self, item):
+        self.items.append(item)
+
+    # Agregamos dinero al inventario
+    def add_money(self, money):
+        self.money += money
+
+    # Actualizamos la posición del jugador en tiempo real
+    def mover(self, nueva_posicion):
+        self.location = nueva_posicion
+        self.mapa.pos_actual = nueva_posicion
 
 
 myPlayer = Player()
-
-
-def title_screen_selections():
-    option = input('')
-    if option.lower() == 'play':
-        start_game()
-    elif option.lower() == 'help':
-        help_menu()
-    elif option.lower() == 'quit':
-        sys.exit()
-
-    while option.lower() not in ['play', 'help', 'quit']:
-        print('Please enter a valid command...')
-        option = input('> ')
-        if option.lower() == 'play':
-            start_game()
-        elif option.lower() == 'help':
-            help_menu()
-        elif option.lower() == 'quit':
-            sys.exit()
-
-
-def title_screen():
-    print('')
-    print('############################')
-    print('# Welcome to ------------- #')
-    print('############################')
-    print('          - Play -          ')
-    print('          - Help -          ')
-    print('          - Quit -          ')
-    print('                            ')
-    title_screen_selections()
-
-
-def help_menu():
-    print('')
-    print('############################')
-    print('# Welcome to ------------- #')
-    print('############################')
-    print('- Use up, down, left, right to move')
-    print('- Type your command to do them')
-    print('- Use "look" to inspect something')
-    print('- Good luck and have fun')
-    title_screen_selections()
-
-
-def start_game():
-    player_name = input('\nHello there, What is your name? ')
-    myPlayer = Player(name=player_name)
-    print('That\'s a nice name')
-    print(f'Welcome, {myPlayer.name}! Let\'s start the game.')
-
-
-def ruta_1():
-    pass
-
-
-def ruta_2():
-    pass
-
-
-def ruta_3():
-    pass
-
-
-def ruta_4():
-    pass
-
-
-title_screen()
-
